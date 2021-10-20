91bb4c8655d0bbb348dcbebcc0d77fa72bb3f6ab
Steven A Moore <stevenallenmoore@gmail.com>
2021-10-20 09:50:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,7 @@
 
 def main():
     #Create an instance on your controller object
+    #Call your mainloop
+    
+    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 2 LINES OF CODE ######
 main()
